ed8211a44b9b343d6af3ad33241f93894c164b49
David Ackerman <david.s.ackerman@gmail.com>
2019-09-23 15:50:14 -0600

    add python-api methods to use new server endpoints
    This allows listing, getting properties of, and deleting top level
    transformations. There's some duplication creeping in here that
    I'm planning to resolve by introducing an intermediate API class.
    
    That's still going to leave some duplication, though. This other
    duplication is resulting from trying to deal with name uniqueness
    constraints in two places. On the Python API side, we're doing
    extra queries and contortions to prevent triggering GraphQL errors,
    and on the GraphQL side, we're basically retracing a bunch of the
    same steps to make sure that the user isn't trying to spoof something.
    
    If we switched on the API side to requiring an organization prefix to
    anything using a name instead of a uuid ('some_org:some_dataset' or
    'some_org:SomeTransformation'), we'd be able to relieve some of
    the burden on the client side and reduce some API parameter bloat.
    
    I want to think on this a bit, and I certainly don't want to muck up
    this PR with it.

--- adi/transformation.py
+++ adi/transformation.py
@@ -1,7 +1,7 @@
 import os
 from typing import Dict
 
-from .common import gql_query, read_code
+from .common import gql_query, read_code, is_uuid
 from . import organization
 
 
@@ -40,3 +40,78 @@ class TransformationAPI:
     result = gql_query(query, variables=variables, connection=self.__connection)
     
     return result['createTransformationTemplate']
+
+  def meta(self, uuid_or_name):
+    transformationUuid = None
+    transformationName = None
+    
+    if not is_uuid(uuid_or_name):
+      transformationName = uuid_or_name
+    else:
+      transformationUuid = uuid_or_name
+
+    query = '''
+    query ($org: OrganizationRef, $transformationUuid: String, $transformationName: String) {
+      transformation(org: $org, uuid: $transformationUuid, name: $transformationName) {
+        id
+        name
+        uuid
+      }
+    }
+    '''
+
+    variables = dict(
+      org = self.__default_organization(),
+      transformationUuid = transformationUuid,
+      transformationName = transformationName
+    )
+
+    results = gql_query(query, variables=variables, connection=self.__connection)
+
+    return results['transformation']
+
+  def delete(self, uuid_or_name):
+    uuid = self.__resolve_to_uuid(uuid_or_name)
+    if not uuid:
+      raise ValueError("Can't find the transformation to delete")
+
+    query = '''
+    mutation DeleteTransformation($uuid: String!) {
+      deleteTransformation(uuid: $uuid)
+    }
+    '''
+    result = gql_query(query, variables={'uuid':uuid}, connection=self.__connection)
+    
+    return result['deleteTransformation']
+
+  def list(self):
+    query = '''
+    query ($org: OrganizationRef!) {
+      transformations(org: $org) {
+        id
+        name
+        uuid
+      }
+    }
+    '''
+    
+    variables = dict(
+      org = self.__default_organization()
+    )
+    
+    results = gql_query(query, variables=variables, connection=self.__connection)
+
+    return results['transformations']
+
+  def __default_organization(self):
+    return self.__connection.organization.default()
+
+  def __resolve_to_uuid(self, uuid_or_name):
+    if not is_uuid(uuid_or_name):
+      info = self.meta(uuid_or_name)
+      if info:
+        return info['uuid']
+      else:
+        return None
+    else:
+      return uuid_or_name
