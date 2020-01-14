1a0ab39cdebfbef7b734e8c1fae230856f003889
David Ackerman <david.ackerman@cybera.ca>
2020-01-14 15:59:03 -0700

    basic rudimentary testing
    This defines a custom TestTransformation class so that we don't have to
    worry about getting data from swift just yet and can test just the main
    logic. Right now, we're just writing results to the console. Yet to be
    done: Use the same python test package that ADI integration tests use.
    
    First we test that we can use a "transformation" just like a normal
    function. Then we test that we can pass parameters into a .run function
    attached to it, just as ADI would. Finally, we're testing that we can
    figure out, from just a block of code, where our actual
    "transformations" are programatically. Whatever bootstraps the running
    of these transformations will need to know that, after all.

--- test/main.py
+++ test/main.py
@@ -1,3 +1,34 @@
-import adi
+import os
+import pandas as pd
+from adi.dev.transformation import Transformation, transformation
 
-print("Hello world")
+def test_loader(dataptr):
+  path = os.path.join("/usr/src/pkg/test/data", dataptr)
+  return pd.read_csv(path)
+
+def test_writer(df, dataptr):
+  path = os.path.join("/usr/src/pkg/test/data/output", dataptr)
+  df.to_csv(path)
+
+class TestTransformation(Transformation):
+  def __init__(self, transform_func, loader={}, default_loader=None, writer=None):
+    super().__init__(transform_func, loader, test_loader, test_writer)
+
+@transformation(TestTransformation)
+def first10(iris):
+  return iris.head(10)
+
+iris = pd.read_csv("/usr/src/pkg/test/data/iris.csv")
+print(first10(iris))
+
+first10.run({
+  "input": {
+    "iris": "iris.csv"
+  },
+  "output": "iris-first10.csv"
+})
+
+keys = list(globals().keys())
+for key in keys:
+  if isinstance(globals()[key], Transformation):
+    print(key)
