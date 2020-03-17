ad9fc4303de213ff615c2a6fa104068e3796d87b
David Ackerman <david.ackerman@cybera.ca>
2020-03-16 21:52:33 -0600

    make @transformation actually work with ADI
    There was a lot of experimentation and juggling here, and it's hard to
    make the changes look pretty. The current tests define some of the
    input/output expectations when transform code is run from ADI (as
    opposed to a simple function call while testing in a Jupyter notebook).
    
    There's a lot of thought towards the future here. In the end, the
    decorated transformation function gets a .run method attached,
    which ADI expects to be able to pass a strongly structured parameter
    map. A .metadata variable is attached as well, allowing the enclosing
    transformation class to write to it pretty much any way it wants (in
    the default Transformation, we just run the results through some
    pandas functions to collect column info). Finally, .inputs is attached
    to record the results of any mappings made while defining the
    transformation (for non-reusable transformations).
    
    More explicit documentation to follow in the manual.

--- adi/dev/transformation.py
+++ adi/dev/transformation.py
@@ -3,46 +3,36 @@ import os
 import pandas as pd
 import requests
 import hashlib
+from . import storage
 
-def resource_hash(datamap):
-  return hashlib.sha256(datamap['value'].encode('utf-8')).hexdigest()
+SAMPLE_SIZE=100
 
-def default_loader(datamap):
+def default_loader(datamap, variant='imported'):
   if datamap['storage'] == 'swift-tempurl':
-    ref = resource_hash(datamap)
-    tmppath = f"/tmp/adi/{ref}"
-    urlstream = requests.get(datamap['value'], stream=True)
-    # TODO: Check if tmppath exists already before downloading to it?
-    with open(tmppath, 'wb') as fd:
-      for chunk in urlstream.iter_content(chunk_size):
-        fd.write(chunk)
-    
     if datamap['format'] == 'csv':
-      return pd.read_csv(path)
+      return storage.read_csv(datamap['value'][variant])
     else:
-      raise Exception(f"Don't know how to deal with format: {datamap['format']}")
+      return storage.read_raw(datamap['value'][variant])
   else:
     raise Exception(f"Don't know how to deal with storage: {datamap['storage']}")
 
-def default_writer(df, datamap):
+def default_writer(data, datamap, variant='imported'):
   if datamap['storage'] == 'swift-tempurl':
-    ref = resource_hash(datamap)
-    tmppath = f"/tmp/adi/{ref}"
-
+    # TODO: Should probably figure out from the return type and not the passed in format
     if datamap['format'] == 'csv':
-      # TODO: Check if tmppath exists already before writing to it?
-      df.to_csv(tmppath)
-      requests.put(datamap['value'], data=open(tmppath, 'rb'))
+      storage.write_csv(data, datamap['value'][variant])
     else:
-      raise Exception(f"Don't know how to deal with format: {datamap['format']}")
+      storage.write_raw(data, datamap['value'][variant])
   else:
     raise Exception(f"Don't know how to deal with storage: {datamap['storage']}")
 
 class Transformation:
-  def __init__(self, transform_func, loader={ 'default': default_loader }, writer=default_writer):
+  def __init__(self, transform_func, loader={ 'default': default_loader }, writer=default_writer, inputs={}):
     self.transform_func = transform_func
     self.loader = loader
     self.writer = writer
+    self.inputs = inputs
+    self.metadata = dict()
 
   def __call__(self, *args, **kwargs):
     return self.transform_func(*args, **kwargs)
@@ -53,11 +43,18 @@ class Transformation:
     loaded_params = {}
 
     for k,v in input_params.items():
+      # Get default values set when creating the transformation. All of these
+      # can potentially be overridden by ADI.
+      if k in self.inputs and isinstance(self.inputs[k], dict):
+        datamap = { **self.inputs[k], **v }
+      else:
+        datamap = v
+
       if k in self.loader:
         print(f"Specialized loader for {k}")
-        loaded_params[k] = self.loader[k](v)
+        loaded_params[k] = self.loader[k](datamap)
       else:
-        loaded_params[k] = self.loader['default'](v)
+        loaded_params[k] = self.loader['default'](datamap)
   
     return loaded_params
 
@@ -65,16 +62,44 @@ class Transformation:
     return self.transform_func(**inputs)
 
   def output(self, result, outputptr):
-    print(f"outputting: {result}")
+    print(f"output: {outputptr}")
     self.writer(result, outputptr)
+
+  def sample(self, result):
+    sample_data = ""
+    if type(result) is pd.DataFrame:
+      sample_size = min(result.shape[0], SAMPLE_SIZE)
+      sample_data = result.sample(sample_size)
+    return sample_data
+    
+  def output_sample(self, result, outputptr):
+    sample_data = self.sample(result)
+    self.writer(sample_data, outputptr, variant='sample')
+
+  def record_result_metadata(self, result):
+    if type(result) is pd.DataFrame:
+      column_types = result.dtypes
+      columns = [dict(name=name,
+                      originalName=name,
+                      tags=[convert_type(column_types[name])],
+                      order=i+1) for i, name in enumerate(result.columns)]
+      self.metadata['columns'] = columns
+      self.metadata['type'] = 'csv'
+    else:
+      self.metadata['type'] = 'document'
+
+  def record_storage_metadata(self, outputptr):
+    self.metadata['bytes'] = storage.bytes(outputptr['value']['imported'])
 
   def run(self, params):
     loaded = self.load(params['input'])
     results = self.transform(loaded)
     self.output(results, params['output'])
-    return results
+    self.output_sample(results, params['output'])
+    self.record_result_metadata(results)
+    self.record_storage_metadata(params['output'])
 
-def transformation(TransformationClass=Transformation, loader={}):
+def transformation(TransformationClass=Transformation, loader={ 'default': default_loader }, inputs={}):
   # Note that to test for functions, the normal recommended route is using
   # callable(suspected_function). But in our case, we're using __call__ in
   # the Transformation class itself, so when we really want to test for a
@@ -87,9 +112,28 @@ def transformation(TransformationClass=Transformation, loader={}):
   # make things feel slicker for our wonderful data scientists.
   if isinstance(TransformationClass, FunctionType):
     print("calling decorator with no args")
-    return Transformation(TransformationClass)
+    return Transformation(TransformationClass, inputs=inputs)
   else:
     def wrap(transform_func):
-      t = TransformationClass(transform_func, loader)
+      t = TransformationClass(transform_func, loader, inputs=inputs)
       return t
     return wrap
+
+def dataset(name, org=None, variant="imported"):
+  if org:
+    name = f"{org}:{name}"
+
+  return { 'ref': name, 'reftype': 'dataset', 'variant': variant }
+
+def convert_type(pd_type):
+  if pd_type == 'object':
+    return 'String'
+  elif pd_type == 'int64':
+    return 'Integer'
+  elif pd_type == 'float64':
+    return 'Float'
+  elif pd_type == 'bool':
+    return 'Boolean'
+  else:
+    # String is a good default if nothing more specific can be found
+    return 'String'
